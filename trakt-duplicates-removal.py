c92e347a16a4e20062c4e434d9c88ff8f14eb575
h3llrais3r <pooh_beer_1@hotmail.com>
2022-11-25 23:23:13 +0100

    Fix duplicate removal and list watched movies & shows
    - Fix duplicate removal based on type
    - Fix duplicate removal for keep_per_day
    - Save list of watched movies and shows

--- trakt-duplicates-removal.py
+++ trakt-duplicates-removal.py
@@ -9,8 +9,8 @@ username = 'YOUR USERNAME'
 
 # Optional
 types = ['movies', 'episodes']  # 'movies' or 'episodes' or both
-shows = []  # list of shows for which to remove duplicates, empty means remove duplicates for all shows
 movies = [] # list of movies which to remove duplicates, empty means remove duplicates for all movies
+shows = []  # list of shows for which to remove duplicates, empty means remove duplicates for all shows
 keep_per_day = False  # set to True to keep one entry per day
 remove = False  # True will remove the duplicates, False will only do a dry run and list the duplicates
 
@@ -19,6 +19,8 @@ trakt_api = 'https://api.trakt.tv'
 auth_get_token_url = '%s/oauth/token' % trakt_api
 get_history_url = '%s/users/%s/history/{type}?page={page}&limit={limit}' % (trakt_api, username)
 sync_history_url = '%s/sync/history/remove' % trakt_api
+get_watched_movies_url = '%s/sync/watched/movies' % trakt_api
+get_watched_shows_url = '%s/sync/watched/shows' % trakt_api
 
 session = requests.Session()
 
@@ -57,6 +59,42 @@ def login_to_trakt():
     })
 
 
+def get_watched_movies():
+    results = []
+
+    print('Retrieving watched movies')
+    resp = session.get(get_watched_movies_url)
+
+    if resp.status_code != 200:
+        print(resp)
+
+    results = resp.json()
+    with open('movies_watched.json', 'w') as output:
+            json.dump(results, output, indent=4)
+            print('Watched movies saved in file movies_watched.json')
+
+    print('Done retrieving watched movies')
+    print()
+
+
+def get_watched_shows():
+    results = []
+
+    print('Retrieving watched shows')
+    resp = session.get(get_watched_shows_url)
+
+    if resp.status_code != 200:
+        print(resp)
+
+    results = resp.json()
+    with open('shows_watched.json', 'w') as output:
+            json.dump(results, output, indent=4)
+            print('Watched shows saved in file shows_watched.json')
+
+    print('Done retrieving watched shows')
+    print()
+
+
 def get_history(type):
     results = []
 
@@ -84,6 +122,12 @@ def get_history(type):
             break
 
     print('Done retrieving %s history' % type)
+    
+    with open('%s_history.json' % type, 'w') as output:
+        json.dump(results, output, indent=4)
+        print('History saved in file %s_history.json' % type)
+        print()
+    
     return results
 
 
@@ -97,8 +141,8 @@ def remove_duplicate(history, type):
 
     for i in history[::-1]:
         if i[entry_type]['ids']['trakt'] in entries:
-            if not keep_per_day or i['watched_at'].split('T')[0] == entries.get(i[entry_type]['ids']['trakt']):
-                if i[entry_type] == 'movie':
+            if not keep_per_day or i['watched_at'].split('T')[0] in entries.get(i[entry_type]['ids']['trakt']):
+                if i['type'] == 'movie':
                     if movies and len(movies) > 0:
                         if i['movie']['title'] in movies:
                             duplicates.append(i['id'])
@@ -106,7 +150,7 @@ def remove_duplicate(history, type):
                     else:
                         duplicates.append(i['id'])
                         print('Duplicate found for %s' % (i['movie']['title']))
-                if i[entry_type] == 'episode':
+                if i['type'] == 'episode':
                     if shows and len(shows) > 0:
                         if i['show']['title'] in shows:
                             duplicates.append(i['id'])
@@ -114,8 +158,10 @@ def remove_duplicate(history, type):
                     else:
                         duplicates.append(i['id'])
                         print('Duplicate found for %s season %s episode %s' % (i['show']['title'], i['episode']['season'], i['episode']['number']))
+            else:
+                entries[i[entry_type]['ids']['trakt']].append(i['watched_at'].split('T')[0])  # add to list with watch dates
         else:
-            entries[i[entry_type]['ids']['trakt']] = i['watched_at'].split('T')[0]
+            entries[i[entry_type]['ids']['trakt']] = [i['watched_at'].split('T')[0]]  # create initial list with watch dates
 
     if len(duplicates) > 0:
         print('%s %s duplicates plays to be removed' % (len(duplicates), type))
@@ -130,11 +176,12 @@ def remove_duplicate(history, type):
 if __name__ == '__main__':
     login_to_trakt()
 
+    if 'movies' in types:
+        get_watched_movies()
+    
+    if 'episodes' in types:
+        get_watched_shows()
+
     for type in types:
         history = get_history(type)
-        with open('%s.json' % type, 'w') as output:
-            json.dump(history, output, indent=4)
-            print('History saved in file %s.json' % type)
-
         remove_duplicate(history, type)
-        print()
